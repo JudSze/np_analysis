aa198b3c3f7fbd8965efe18225874813146719d0
JudSze <{ID}+{username}@users.noreply.github.com>
2025-08-06 17:00:22 +0200

    figured tables and columns in the db

--- retrieve_mibig_data/create_structure_db.py
+++ retrieve_mibig_data/create_structure_db.py
@@ -8,25 +8,27 @@ import sqlite3
 
 from rdkit import Chem
 
+# Paths and files
 mibig_json_path = "/home/szenei/np_analysis/retrieve_mibig_data/mibig_json_4.0"
 mibig_json_files = [file for file in listdir(mibig_json_path) if isfile(join(mibig_json_path, file))]
-
-lwreg_config_path = "/home/szenei/np_analysis/retrieve_mibig_data/lwreg_db_config.json"
-
-connection = sqlite3.connect("/home/szenei/np_analysis/mibig_structures")
-cursor = connection.cursor()
-
-cursor.execute(f"ALTER TABLE registry ADD COLUMN BGC TEXT")
-
-# Get tables from database
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-cursor.fetchall()   # regiostration_metadata, hashes, orig_data, molblocks
 
 # Init lwreg-db
 # DO NOT RUN IT, RUNNING IT RESETS THE WHOLE DB
 # lwreg_cofig_handle = open(lwreg_config_path, "r")
 # lwreg_config = json.load(lwreg_cofig_handle)
 # lwreg.utils.initdb(lwreg_config)
+
+lwreg_config_path = "/home/szenei/np_analysis/retrieve_mibig_data/lwreg_db_config.json"
+
+# DB manipulation
+connection = sqlite3.connect("/home/szenei/np_analysis/mibig_structures")
+cursor = connection.cursor()
+
+try:
+    cursor.execute(f"ALTER TABLE orig_data ADD COLUMN BGC TEXT")
+except Warning:
+    print("Column already exists")
+
 
 # Go into MIBiG json data
 for file in mibig_json_files:
@@ -35,7 +37,7 @@ for file in mibig_json_files:
         accession = accession
 
         structures = []
-        numerator = 0
+        id = 1
         # Get SMILES from the MIBiG entries
         for compound in entry["compounds"]:
             try:
@@ -43,7 +45,7 @@ for file in mibig_json_files:
                 molecule = Chem.MolFromSmiles(compound)
                 # Register moleculer in lwreg
                 lwreg.utils.register(lwreg_config_path, mol = molecule, smiles=compound)
-                cursor.execute(f"ALTER TABLE mibig_structures ADD COLUMN {accession}")
-                numerator += 1
+                cursor.execute(f'UPDATE orig_data SET BGC={accession} WHERE molregno={id}')
+                id += 1
             except KeyError:
                 print("no structure available")
